b458ad43dbc1f73e1950ac42ff749b2367062ff8
AeroSoap <joshua.adler@digipen.edu>
2019-11-08 13:37:57 -0800

    AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH

---
+++ Pong/Assets/Scripts/SetWinner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetWinner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Text txt = GetComponent<Text>();
+        txt.text = "The " + BallOffScreen.Winner + " player wins!";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
