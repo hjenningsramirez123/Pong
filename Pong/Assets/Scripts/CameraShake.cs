26cdae8ce1464c7f37c665caff9a1b52af8ada9e
AeroSoap <joshua.adler@digipen.edu>
2019-11-13 14:04:37 -0800

    Fixed a bug.

--- Pong/Assets/Scripts/CameraShake.cs
+++ Pong/Assets/Scripts/CameraShake.cs
@@ -14,7 +14,7 @@ public class CameraShake : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        time = Seconds;
+        time = Seconds + Offset;
     }
 
     public void Reset()
