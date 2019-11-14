2aa130221730e5f64835ee0ae2fde576807b1ccb
hjenningsramirez123 <h.jenningsramirez@digipen.edu>
2019-11-14 12:55:49 -0800

    Fixed bug in original

---
+++ Pong/Assets/Scripts/BallOffScreenOriginal.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class BallOffScreenOriginal : MonoBehaviour
+{
+    public Text TextLeft;
+    public Text TextRight;
+    public GameObject cam;
+
+    public static string Winner = null;
+
+    private Rigidbody2D myRB;
+    private BallInitMove init;
+
+    int scoreLeft, scoreRight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        init = GetComponent<BallInitMove>();
+        myRB = GetComponent<Rigidbody2D>();
+        scoreLeft = 0;
+        scoreRight = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // If the ball is on the edge of the screen
+        if(Mathf.Abs(transform.position.x) > 10)
+        {
+            // Increment score for the correct player
+            if (transform.position.x > 0)
+            {
+                scoreLeft++;
+                TextLeft.text = scoreLeft.ToString();
+                // Check if that player has won
+                if(scoreLeft == 11)
+                {
+                    Winner = "left";
+                }
+            }
+            else
+            {
+                scoreRight++;
+                TextRight.text = scoreRight.ToString();
+                if(scoreRight == 11)
+                {
+                    Winner = "right";
+                }
+            }
+            // Make the camera shake
+            if(Winner == null)
+            {
+                // Reset the ball's position and make it idle for Delay before moving
+                transform.position = new Vector3(0, -100, 0);
+                myRB.velocity = new Vector3(0, 0, 0);
+                init.StartCoroutine("Initialize");
+            } else
+            {
+                // If there has been a winner determined, switch to the Win scene
+                SceneManager.LoadScene("Win");
+            }
+        }
+    }
+}
